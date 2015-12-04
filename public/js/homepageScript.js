93274f14f13c9405153a28f90978b2cb279cc902
SanderBossuyt <sander.bossuyt@student.howest.be>
2015-12-04 18:50:41 +0100

    homepaginahtml css js, styling ui gamepage

---
+++ public/js/homepageScript.js
@@ -0,0 +1,44 @@
+var checkone = true;
+var checktwo;
+
+var firstinput = document.getElementsByClassName("firstinput");
+var gamemode = document.getElementsByClassName("gamemode");
+
+
+
+  for (i = 0; i < firstinput.length; i++) {
+    firstinput[i].addEventListener("click", function(e){
+
+      e.preventDefault();
+      if (checkone === true) {
+        //console.log(e.currentTarget.value);
+      $('.controls').removeClass('hidden');
+      $('.controls').addClass('unhidden');
+      e.currentTarget.style.border = '5px solid white';
+      checktwo = true;
+      checkone = false;
+      }
+    });
+  }
+
+
+
+
+  for (i = 0; i < gamemode.length; i++) {
+    gamemode[i].addEventListener("click", function(e){
+      e.preventDefault();
+      if (checktwo === true) {
+      //console.log(e.currentTarget.value);
+      $('.submit').removeClass('hidden');
+      $('.submit').addClass('unhidden');
+      e.currentTarget.style.border = '5px solid white';
+      checktwo = false;
+      }
+    });
+  }
+
+
+
+
+
+
